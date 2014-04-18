9cc78930ce966f91835a9a0b9b9d19f362883e47
Garen Torikian <gjtorikian@gmail.com>
2014-04-18 11:07:50 -0700

    Stub out failures

--- test/mathematical/basic_test.rb
+++ test/mathematical/basic_test.rb
@@ -1,9 +1,14 @@
 require "test_helper"
-require "mathematical"
 
 class Mathematical::BasicTest < Test::Unit::TestCase
 
   def test_it_has_a_version
     assert Mathematical::VERSION
+  end
+
+  def test_it_errors_when_missing_commands
+    render_stub = Mathematical::Render.new
+    render_stub.stubs(:missing_commands).returns(["dvipng"])
+    assert_raise(Mathematical::CommandNotFoundError) { render_stub.render("$a \\ne b$") }
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,4 @@
 require 'bundler/setup'
 require 'mathematical'
 require 'test/unit'
+require 'mocha/test_unit'
