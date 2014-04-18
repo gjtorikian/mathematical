d45227fbbae25eb5c0b365d09b67d37bb1997691
Garen Torikian <gjtorikian@gmail.com>
2014-04-17 23:40:43 -0700

    Simplify testing

--- test/mathematical/basic_test.rb
+++ test/mathematical/basic_test.rb
@@ -5,8 +5,4 @@ class Mathematical::BasicTest < Test::Unit::TestCase
   def test_it_has_a_version
     assert Mathematical::VERSION
   end
-
-  def test_something_prints
-    puts Mathematical::Render.new.render("wow")
-  end
 end
--- test/mathematical/fixtures_test.rb
+++ test/mathematical/fixtures_test.rb
@@ -8,7 +8,7 @@ class Mathematical::FixturesTest < Test::Unit::TestCase
     define_method "test_#{name}" do
       source = File.read(before)
 
-      expected_file = before.sub(/before/, "after")
+      expected_file = before.sub(/before/, "after").sub(/text/, "html")
       expected = File.read(expected_file).rstrip
       actual = Mathematical::Render.new.render(source).rstrip
 
