8685ad05b6aadd3055e7b75d43cfd701b9434662
Garen Torikian <gjtorikian@gmail.com>
2014-04-21 14:50:54 -0700

    Add big performance tests

---
+++ test/mathematical/performance_test.rb
@@ -0,0 +1,10 @@
+require "test_helper"
+require 'benchmark'
+
+class Mathematical::BasicTest < Test::Unit::TestCase
+
+  # def test_it_handles_big_files
+  #   big_file = File.read('test/mathematical/fixtures/performance/big_file.text')
+  #   puts Benchmark.measure { Mathematical::Render.new.render(big_file) }
+  # end
+end
