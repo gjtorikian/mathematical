53dfd587208e5987ae73867901570f099794ee94
Garen Torikian <gjtorikian@gmail.com>
2014-04-21 15:51:31 -0700

    Assert proper performance test

--- test/mathematical/performance_test.rb
+++ test/mathematical/performance_test.rb
@@ -3,8 +3,10 @@ require 'benchmark'
 
 class Mathematical::BasicTest < Test::Unit::TestCase
 
-  # def test_it_handles_big_files
-  #   big_file = File.read('test/mathematical/fixtures/performance/big_file.text')
-  #   puts Benchmark.measure { Mathematical::Render.new.render(big_file) }
-  # end
+  def test_it_handles_big_files
+    big_file = File.read('test/mathematical/fixtures/performance/big_file.text')
+    speed = Benchmark.realtime { Mathematical::Render.new.render(big_file) }
+    # speed = speed.match /\(\s*(\S)\s*\)/
+    assert_operator speed, :<=, 5
+  end
 end
