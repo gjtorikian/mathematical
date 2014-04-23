5b6ece159f206a3d9f68ee40676d49e5273a5914
Garen Torikian <gjtorikian@gmail.com>
2014-04-23 00:42:16 -0700

    Tabs. SPACES.

--- ext/mathematical/mathematical.c
+++ ext/mathematical/mathematical.c
@@ -29,56 +29,52 @@ GLOBAL(raster,*bitmap_raster,NULL);
 
 static VALUE rb_mMathematical;
 static VALUE rb_cMathematicalProcess;
-STATIC	FILE *msgfp;
+STATIC  FILE *msgfp;
 
-int	MATHEMATICAL_GetPixel ( int x, int y ) {
-	int	ipixel = y*bitmap_raster->width + x; /* pixel index for x,y-coords*/
-	return (int)getlongbit(bitmap_raster->pixmap,ipixel); /*pixel = 0 or 1*/;
+int  MATHEMATICAL_GetPixel ( int x, int y ) {
+  int  ipixel = y*bitmap_raster->width + x; /* pixel index for x,y-coords*/
+  return (int)getlongbit(bitmap_raster->pixmap,ipixel); /*pixel = 0 or 1*/;
 }
 
 static VALUE MATHEMATICAL_init(VALUE self, int fontSize) {
-	rb_iv_set(self, "@fontSize", fontSize);
-	return self;
+  rb_iv_set(self, "@fontSize", fontSize);
+  return self;
 }
 
-static VALUE MATHEMATICAL_process(VALUE self, VALUE rb_sLatexCode, VALUE rb_sFilename) {
-	Check_Type (rb_sLatexCode, T_STRING);
+static VALUE MATHEMATICAL_process(VALUE self, VALUE rb_sLatexCode) {
+  Check_Type (rb_sLatexCode, T_STRING);
 
-	VALUE rb_sLatexCodeCopy;
+  VALUE rb_sLatexCodeCopy;
 
-	// Because plain duplication shares internal memory region.  You
-	// have to call rb_str_modify() before modifying a string.
+  // Because plain duplication shares internal memory region.  You
+  // have to call rb_str_modify() before modifying a string.
   rb_sLatexCodeCopy = rb_str_dup(rb_sLatexCode);
   rb_str_modify(rb_sLatexCodeCopy);
 
-	int fontSize = FIX2INT(rb_iv_get(self, "@fontSize"));
+  int fontSize = FIX2INT(rb_iv_get(self, "@fontSize"));
 
-	if(fontSize > 4 || fontSize < 1) fontSize = 2;
+  if(fontSize > 4 || fontSize < 1) fontSize = 2;
 
-	subraster *sp = rasterize(StringValuePtr(rb_sLatexCodeCopy), fontSize);
-	bitmap_raster = sp->image;
+  subraster *sp = rasterize(StringValuePtr(rb_sLatexCodeCopy), fontSize);
+  bitmap_raster = sp->image;
 
-  // type_raster(bitmap_raster,stdout); /* display ascii image of raster */
+  int gifSize = 0;
+  int status = GIF_Create(NULL, bitmap_raster->width, bitmap_raster->height, 2, 8);
+  GIF_SetColor(1,0,0,0); /* foreground black if all 0 */
 
-	// printf(rb_sFilename);
+  if(status != 0) rb_raise(INT2NUM(status), "Could not create GIF image");
+  GIF_SetTransparent(0);
+  GIF_CompressImage(0, 0, -1, -1, MATHEMATICAL_GetPixel); /* emit gif */
+  gifSize = GIF_Close();
 
-	int gifSize = 0;
-	int status = GIF_Create("/Users/garentorikian/Development/mathematical/out.gif", bitmap_raster->width, bitmap_raster->height, 2, 8);
-	GIF_SetColor(1,0,0,0); /* foreground black if all 0 */
+  if(sp != NULL) delete_subraster(sp);  /* and free expression */
 
-	if(status != 0) rb_raise(INT2NUM(status), "Could not create GIF image");
-	GIF_SetTransparent(0);
-	GIF_CompressImage(0, 0, -1, -1, MATHEMATICAL_GetPixel); /* emit gif */
-	gifSize = GIF_Close();
-
-	if(sp != NULL) delete_subraster(sp);	/* and free expression */
-
-	return rb_str_new("wow", 3);
+  return rb_str_new("wow", 3);
 }
 
 void Init_mathematical() {
-	rb_mMathematical = rb_define_module("Mathematical");
-	rb_cMathematicalProcess = rb_define_class_under(rb_mMathematical, "Process", rb_cObject);
-	rb_define_method(rb_cMathematicalProcess, "initialize", MATHEMATICAL_init, 1);
-	rb_define_method(rb_cMathematicalProcess, "process", MATHEMATICAL_process, 2);
+  rb_mMathematical = rb_define_module("Mathematical");
+  rb_cMathematicalProcess = rb_define_class_under(rb_mMathematical, "Process", rb_cObject);
+  rb_define_method(rb_cMathematicalProcess, "initialize", MATHEMATICAL_init, 1);
+  rb_define_method(rb_cMathematicalProcess, "process", MATHEMATICAL_process, 1);
 }
