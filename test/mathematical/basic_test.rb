34dd69ae78687b32663d5566e5ddf435ddcd4995
Garen Torikian <gjtorikian@gmail.com>
2014-04-23 17:41:05 -0700

    Cleanup of various tests

--- test/mathematical/basic_test.rb
+++ test/mathematical/basic_test.rb
@@ -6,16 +6,10 @@ class Mathematical::BasicTest < Test::Unit::TestCase
     assert Mathematical::VERSION
   end
 
-  def test_it_errors_when_missing_commands
-    render_stub = Mathematical::Render.new
-    render_stub.stubs(:missing_commands).returns(["dvipng"])
-    assert_raise(Mathematical::CommandNotFoundError) { render_stub.render("$a \\ne b$") }
-  end
-
   def test_it_does_not_error_on_unrecognized_commands
     render_stub = Mathematical::Render.new
     output = nil
-    assert_nothing_raised { output = render_stub.render("$\\qwerty$") }
-    assert_equal output, "$\\qwerty$"
+    assert_nothing_raised { output = render_stub.render('$\h$') }
+    assert_equal output, '$\h$'
   end
 end
--- test/mathematical/performance_test.rb
+++ test/mathematical/performance_test.rb
@@ -6,7 +6,6 @@ class Mathematical::BasicTest < Test::Unit::TestCase
   def test_it_handles_big_files
     big_file = File.read('test/mathematical/fixtures/performance/big_file.text')
     speed = Benchmark.realtime { Mathematical::Render.new.render(big_file) }
-    # speed = speed.match /\(\s*(\S)\s*\)/
     assert_operator speed, :<=, 5
   end
 end
