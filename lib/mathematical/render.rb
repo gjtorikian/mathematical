c9235a48d00cf194574b980600a26f85527fdb6c
Garen Torikian <gjtorikian@gmail.com>
2014-05-01 01:10:49 -0700

    Trash previous code to focus on simplicity of lasem

--- lib/mathematical/render.rb
+++ lib/mathematical/render.rb
@@ -1,117 +1,49 @@
-require 'rbconfig'
-require 'fileutils'
-require 'tmpdir'
-require 'shellwords'
-require 'base64'
-
 module Mathematical
   class Render
     DEFAULT_OPTS = {
-      :html_png_resolution => 200
+
     }
 
     def initialize(opts = {})
-      @os ||= (
-        host_os = RbConfig::CONFIG['host_os']
-        case host_os
-        when /darwin|mac os/
-          "macosx"
-        when /linux/
-          "linux"
-        else
-          raise Error, "unknown os: #{host_os.inspect}"
-        end
-      )
-
-      opts = DEFAULT_OPTS.merge(opts)
-      @temp_dir = Dir.mktmpdir
-      @args = [
-              '--png',
-              '--use-preview-package',
-              # '--shell-dvipng',
-              # "'dvipng -D #{Shellwords.shellescape opts[:html_png_resolution].to_s}'",
-             ]
+      @config = DEFAULT_OPTS.merge(opts)
+      @processer = Mathematical::Process.new(2)
     end
 
 
     def render(text)
-      in_tmpdir do |tmpdir|
-        @args << "--temp-directory #{Shellwords.shellescape tmpdir.first}"
-        @args << "--png-directory #{Shellwords.shellescape tmpdir.first}"
-
-        text.gsub(Mathematical::Parser::REGEX) do |maths|
-          if maths =~ /^\$(?!\$)/
-            just_maths = maths[1..-2]
-            type = :inline
-          elsif maths =~ /^\\\((?!\\\[)/
-            just_maths = maths[2..-4]
-            type = :inline
-          elsif maths =~ /^\\\[(?!\\\[)/
-            just_maths = maths[2..-4]
-            type = :display
-          elsif maths =~ /^\\begin(?!\\begin)/
-            just_maths = maths[16..-15]
-            type = :display
-          end
-
-          data = run_blahtex(just_maths, type)
-
-          if error = data.match("<error>(.+?)</error>")
-            if data.match("<message>Unrecogni[sz]?ed command")
-              return maths
-            else
-              raise ParseError, error
-            end
-          elsif filename = data.match("<md5>(.+?)</md5>")
-            filename = filename[1]
-            depth = data.match("<depth>(.+?)</depth>")[1]
-            "<img class=\"#{type.to_s}-math\" style=\"vertical-align: #{depth}px\" src=\"data:image/png;base64,#{png_to_base64(File.join(tmpdir, "#{filename}.png"))}\"/>"
-          end
+      text.gsub(Mathematical::Parser::REGEX) do |maths|
+        if maths =~ /^\$(?!\$)/
+          just_maths = maths[1..-2]
+          type = :inline
+        elsif maths =~ /^\\\((?!\\\[)/
+          just_maths = maths[2..-4]
+          type = :inline
+        elsif maths =~ /^\\\[(?!\\\[)/
+          just_maths = maths[2..-4]
+          type = :display
+        elsif maths =~ /^\\begin(?!\\begin)/
+          just_maths = maths[16..-15]
+          type = :display
         end
+
+        begin
+          puts @processer.process(just_maths, "sss")
+        rescue RuntimeError
+          return just_maths
+        end
+
+        # if error = data.match("<error>(.+?)</error>")
+        #   if data.match("<message>Unrecogni[sz]?ed command")
+        #     return maths
+        #   else
+        #     raise ParseError, error
+        #   end
+        # elsif filename = data.match("<md5>(.+?)</md5>")
+        #   filename = filename[1]
+        #   depth = data.match("<depth>(.+?)</depth>")[1]
+        #   "<img class=\"#{type.to_s}-math\" style=\"vertical-align: #{depth}px\" src=\"data:image/png;base64,#{png_to_base64(File.join(tmpdir, "#{filename}.png"))}\"/>"
+        # end
       end
-    end
-
-    def run_blahtex(tex, type)
-      raise CommandNotFoundError, "Required commands missing: #{missing_commands.join(', ')} in PATH. (#{ENV['PATH']})" unless missing_commands.empty?
-      @args << '--displaymath' if type == :equation
-
-      IO.popen(["bin/blahtex/#{@os}/blahtex", *@args].join(' '), 'w+') do |blahtex|
-        blahtex.write tex
-        blahtex.close_write
-
-        output = blahtex.read
-        blahtex.close_read
-
-        raise "Error running blahtex" unless $?.success?
-
-        output
-      end
-    end
-
-    def png_to_base64(path)
-      File.open(path, 'r') { |image_file| Base64.encode64(image_file.read).gsub(/\n/, '') }
-    end
-
-private
-
-    # Check toolchain availability and returns array of missing tools
-    def missing_commands
-      commands = []
-      commands << "dvipng" unless can_run?("dvipng -v")
-      commands
-    end
-
-    # Trial command and check if return code is zero
-    def can_run?(command)
-      `#{command} 2>&1`
-      $?.exitstatus.zero?
-    end
-
-    def in_tmpdir
-      path = FileUtils.mkdir_p Dir.tmpdir
-      yield path
-    ensure
-      FileUtils.rm_rf( path )
     end
   end
 end
