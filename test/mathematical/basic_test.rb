13898510cf4675e61957cd5cbe77082e2cb45d55
Garen Torikian <gjtorikian@gmail.com>
2014-05-03 10:40:21 -0700

    Extract separate maliciousness test

--- test/mathematical/basic_test.rb
+++ test/mathematical/basic_test.rb
@@ -6,33 +6,4 @@ class Mathematical::BasicTest < Test::Unit::TestCase
     assert Mathematical::VERSION
   end
 
-  def test_it_does_not_error_on_unrecognized_commands
-    render = Mathematical::Render.new
-    output = nil
-    assert_nothing_raised { output = render.render('$\align$') }
-    assert_equal output, '$\align$'
-  end
-
-  def test_it_does_not_blow_up_on_bad_arguments
-    # need to pass a hash here
-    assert_raise TypeError do
-      render = Mathematical::Render.new("not a hash")
-    end
-
-    # need to pass a string here
-    render = Mathematical::Render.new
-    assert_raise TypeError do
-      Mathematical::Render.new.render(123)
-    end
-  end
-
-  def test_it_does_not_blow_up_on_bad_options
-    assert_raise TypeError do
-      render = Mathematical::Render.new({:ppi => "not a number"})
-    end
-
-    assert_raise TypeError do
-      render = Mathematical::Render.new({:zoom => "not a number"})
-    end
-  end
 end
---
+++ test/mathematical/maliciousness_test.rb
@@ -0,0 +1,34 @@
+require "test_helper"
+
+class Mathematical::MaliciousnessTest < Test::Unit::TestCase
+
+  def test_it_does_not_error_on_unrecognized_commands
+    render = Mathematical::Render.new
+    output = nil
+    assert_nothing_raised { output = render.render('$\align$') }
+    assert_equal output, '$\align$'
+  end
+
+  def test_it_does_not_blow_up_on_bad_arguments
+    # need to pass a hash here
+    assert_raise TypeError do
+      render = Mathematical::Render.new("not a hash")
+    end
+
+    # need to pass a string here
+    render = Mathematical::Render.new
+    assert_raise TypeError do
+      Mathematical::Render.new.render(123)
+    end
+  end
+
+  def test_it_does_not_blow_up_on_bad_options
+    assert_raise TypeError do
+      render = Mathematical::Render.new({:ppi => "not a number"})
+    end
+
+    assert_raise TypeError do
+      render = Mathematical::Render.new({:zoom => "not a number"})
+    end
+  end
+end
