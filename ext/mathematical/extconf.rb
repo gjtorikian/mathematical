f4bf6338d3ae4bddc47aab5dcb641a9412efd6b2
Garen Torikian <gjtorikian@gmail.com>
2014-05-09 08:34:26 -0700

    Assert that the `cairo` library exists

--- ext/mathematical/extconf.rb
+++ ext/mathematical/extconf.rb
@@ -9,6 +9,7 @@ find_header("libxml/parser.h", "/usr/include/libxml2", "/usr/local/include/libxm
 find_header("libxml/xpath.h", "/usr/include/libxml2", "/usr/local/include/libxml2")
 find_header("libxml/xpathInternals.h", "/usr/include/libxml2", "/usr/local/include/libxml2")
 
+have_library("cairo")
 have_library("pangocairo-1.0")
 
 $LDFLAGS += " #{`pkg-config --static --libs glib-2.0 gdk-pixbuf-2.0 cairo pango`}"
