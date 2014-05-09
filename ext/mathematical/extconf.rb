a5e34d06faefd646d9f764ea5d80331b91793e13
Garen Torikian <gjtorikian@gmail.com>
2014-05-09 09:54:45 -0700

    Export PKG_CONFIG_PATH instead
    Fixes Cairo errors, for Mac OSX at least. See http://git.io/P_Ob8w

--- ext/mathematical/extconf.rb
+++ ext/mathematical/extconf.rb
@@ -1,7 +1,6 @@
 require 'mkmf'
 
-pkg_config("glib-2.0")
-pkg_config("gdk-pixbuf-2.0")
+system("export PKG_CONFIG_PATH=/opt/X11/lib/pkgconfig")
 
 have_library("xml2")
 find_header("libxml/tree.h", "/usr/include/libxml2", "/usr/local/include/libxml2")
@@ -9,7 +8,6 @@ find_header("libxml/parser.h", "/usr/include/libxml2", "/usr/local/include/libxm
 find_header("libxml/xpath.h", "/usr/include/libxml2", "/usr/local/include/libxml2")
 find_header("libxml/xpathInternals.h", "/usr/include/libxml2", "/usr/local/include/libxml2")
 
-have_library("cairo")
 have_library("pangocairo-1.0")
 
 $LDFLAGS += " #{`pkg-config --static --libs glib-2.0 gdk-pixbuf-2.0 cairo pango`}"
