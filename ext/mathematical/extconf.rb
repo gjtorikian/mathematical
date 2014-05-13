8025026a48524cb637f14ade6063e1739a06c113
Garen Torikian <gjtorikian@gmail.com>
2014-05-12 22:49:10 -0700

    Oh I guess that is important

--- ext/mathematical/extconf.rb
+++ ext/mathematical/extconf.rb
@@ -13,6 +13,4 @@ have_library("pangocairo-1.0")
 $LDFLAGS += " #{`pkg-config --static --libs glib-2.0 gdk-pixbuf-2.0 cairo pango`}"
 $CFLAGS += " #{`pkg-config --cflags glib-2.0 gdk-pixbuf-2.0 cairo pango`}"
 
-with_cflags("-DGTK_DISABLE_DEPRECATED") do
-  create_makefile("mathematical/mathematical")
-end
+create_makefile("mathematical/mathematical")
