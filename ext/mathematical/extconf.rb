6fa74429ed4e86d80005876cf14ad7541b5fcd37
Garen Torikian <gjtorikian@gmail.com>
2014-05-27 00:55:36 -0700

    Set up subdirectories for Lasem and itexToMML

--- ext/mathematical/extconf.rb
+++ ext/mathematical/extconf.rb
@@ -2,6 +2,9 @@ require 'mkmf'
 require 'rbconfig'
 host_os = RbConfig::CONFIG['host_os']
 
+LASEM_DIR = File.join(File.dirname(__FILE__), "lasem", "src")
+ITEX_DIR = File.join(File.dirname(__FILE__), "itexToMML")
+
 if host_os =~ /darwin|mac os/
   ENV['PKG_CONFIG_PATH'] = "/opt/X11/lib/pkgconfig:#{ENV['PKG_CONFIG_PATH']}"
 end
@@ -14,7 +17,7 @@ find_header("libxml/xpathInternals.h", "/usr/include/libxml2", "/usr/local/inclu
 
 have_library("pangocairo-1.0")
 
-$LDFLAGS += " #{`pkg-config --static --libs glib-2.0 gdk-pixbuf-2.0 cairo pango`}"
-$CFLAGS += " #{`pkg-config --cflags glib-2.0 gdk-pixbuf-2.0 cairo pango`}"
+$LDFLAGS += " #{`pkg-config --static --libs glib-2.0 gdk-pixbuf-2.0 cairo pango`.chomp} -I#{LASEM_DIR} -I#{ITEX_DIR}"
+$CFLAGS += " #{`pkg-config --cflags glib-2.0 gdk-pixbuf-2.0 cairo pango`.chomp} -I#{LASEM_DIR} -I#{ITEX_DIR}"
 
 create_makefile("mathematical/mathematical")
