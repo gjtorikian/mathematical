9d488b7ac80f716390034962ad6a676ede53b762
Garen Torikian <gjtorikian@gmail.com>
2014-12-24 15:53:43 -0800

    Trash stdout message

--- test/mathematical/mathjax_test.rb
+++ test/mathematical/mathjax_test.rb
@@ -6,7 +6,7 @@ class Mathematical::MathJaxTest < Test::Unit::TestCase
 
   MATHJAX_TEST_TEST_DIR = File.join('ext', 'mathematical', 'mtex2MML', 'test', 'fixtures', 'MathJax')
   MATHJAX_TEST_TEX_DIR = File.join(MATHJAX_TEST_TEST_DIR, 'LaTeXToMathML-tex')
-  puts MATHJAX_TEST_TEX_DIR
+
   Dir["#{MATHJAX_TEST_TEX_DIR}/**/*.tex"].each do |tex|
     define_method "test_#{tex}" do
       tex_contents = File.read(tex)
