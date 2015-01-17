85e2432c6f1d2695fdce5f06d480a8e0249f1f4d
Garen Torikian <gjtorikian@gmail.com>
2015-01-17 10:19:42 -0800

    Add new libxml2 path

--- ext/mathematical/extconf.rb
+++ ext/mathematical/extconf.rb
@@ -10,10 +10,10 @@ if host_os =~ /darwin|mac os/
 end
 
 have_library("xml2")
-find_header("libxml/tree.h", "/usr/include/libxml2", "/usr/local/include/libxml2")
-find_header("libxml/parser.h", "/usr/include/libxml2", "/usr/local/include/libxml2")
-find_header("libxml/xpath.h", "/usr/include/libxml2", "/usr/local/include/libxml2")
-find_header("libxml/xpathInternals.h", "/usr/include/libxml2", "/usr/local/include/libxml2")
+find_header("libxml/tree.h", "/include/libxml2", "/usr/include/libxml2", "/usr/local/include/libxml2")
+find_header("libxml/parser.h", "/include/libxml2", "/usr/include/libxml2", "/usr/local/include/libxml2")
+find_header("libxml/xpath.h", "/include/libxml2", "/usr/include/libxml2", "/usr/local/include/libxml2")
+find_header("libxml/xpathInternals.h", "/include/libxml2", "/usr/include/libxml2", "/usr/local/include/libxml2")
 
 # TODO: this is so frakkin' stupid. but I can't seem to get subdirs to compile any other way
 # the `destroy_copies` task, immediately after `compile`, will destroy these files
