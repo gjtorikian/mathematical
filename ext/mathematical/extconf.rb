f945f792ecba60cd5a119588ea49351e17dc5068
Garen Torikian <gjtorikian@gmail.com>
2015-05-01 17:36:46 -0700

    Provide updates for the pure-C mtex2MML

--- ext/mathematical/extconf.rb
+++ ext/mathematical/extconf.rb
@@ -3,13 +3,15 @@ require 'rbconfig'
 host_os = RbConfig::CONFIG['host_os']
 
 LASEM_DIR = File.join(File.dirname(__FILE__), 'lasem', 'src')
-MTEX_DIR = File.join(File.dirname(__FILE__), 'mtex2MML', 'src')
+MTEX2MML_DIR = File.join(File.dirname(__FILE__), 'mtex2MML')
 
 if host_os =~ /darwin|mac os/
   ENV['PKG_CONFIG_PATH'] = "/opt/X11/lib/pkgconfig:#{ENV['PKG_CONFIG_PATH']}"
 end
 
+# pre-compile checks
 have_library('xml2')
+have_library('pangocairo-1.0')
 find_header('libxml/tree.h', '/include/libxml2', '/usr/include/libxml2', '/usr/local/include/libxml2')
 find_header('libxml/parser.h', '/include/libxml2', '/usr/include/libxml2', '/usr/local/include/libxml2')
 find_header('libxml/xpath.h', '/include/libxml2', '/usr/include/libxml2', '/usr/local/include/libxml2')
@@ -20,23 +22,15 @@ find_header('libxml/xpathInternals.h', '/include/libxml2', '/usr/include/libxml2
 FileUtils.cp_r(Dir.glob("#{LASEM_DIR}/*"), File.dirname(__FILE__))
 File.delete(File.join(File.dirname(__FILE__), 'lasemrender.c'))
 
-debug_file = File.join(File.dirname(__FILE__), 'mtex2MML_debug.cc')
-if File.exists?(debug_file)
-  File.delete(File.join(File.dirname(__FILE__), 'mtex2MML_debug.cc'))
-end
-
-# build mtex2MML Bison files
-Dir.chdir(MTEX_DIR) do
+# build mtex2MML library
+Dir.chdir(MTEX2MML_DIR) do
   system 'make'
 end
 
-FileUtils.cp_r(Dir.glob("#{MTEX_DIR}/*.{c,h}"), File.dirname(__FILE__))
-FileUtils.cp_r("#{MTEX_DIR}/deps/.", File.dirname(__FILE__))
-FileUtils.cp_r(Dir.glob("#{MTEX_DIR}/deps/**/*"), File.dirname(__FILE__))
-
-have_library('pangocairo-1.0')
+FileUtils.cp_r(Dir.glob("#{MTEX2MML_DIR}/dist/*.{h}"), File.dirname(__FILE__))
 
 $LDFLAGS << " #{`pkg-config --static --libs glib-2.0 gdk-pixbuf-2.0 cairo pango`.chomp}"
-$CFLAGS << "  -g -O2 -std=c99 #{`pkg-config --cflags glib-2.0 gdk-pixbuf-2.0 cairo pango`.chomp} -I#{LASEM_DIR} -I#{MTEX_DIR}"
+$CFLAGS << " -g -O2 -std=c99 #{`pkg-config --cflags glib-2.0 gdk-pixbuf-2.0 cairo pango`.chomp} -I#{LASEM_DIR}"
+$LOCAL_LIBS << "#{MTEX2MML_DIR}/dist/libmtex2MML.a"
 
 create_makefile('mathematical/mathematical')
