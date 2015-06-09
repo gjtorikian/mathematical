38b8201898b2f9a58fbc67a8bc425aa163fe451d
Garen Torikian <gjtorikian@gmail.com>
2015-06-09 09:00:45 -0700

    Update extconf with new cmake build info

--- ext/mathematical/extconf.rb
+++ ext/mathematical/extconf.rb
@@ -2,8 +2,15 @@ require 'mkmf'
 require 'rbconfig'
 host_os = RbConfig::CONFIG['host_os']
 
+unless find_executable('cmake')
+  $stderr.puts "\n\n\n[ERROR]: cmake is required and not installed. Get it here: http://www.cmake.org/\n\n"
+  exit 1
+end
+
+ROOT_TMP = File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'tmp'))
 LASEM_DIR = File.expand_path(File.join(File.dirname(__FILE__), 'lasem', 'src'))
 MTEX2MML_DIR = File.expand_path(File.join(File.dirname(__FILE__), 'mtex2MML'))
+MTEX2MML_BUILD_DIR = File.join(MTEX2MML_DIR, 'build')
 
 if host_os =~ /darwin|mac os/
   ENV['PKG_CONFIG_PATH'] = "/opt/X11/lib/pkgconfig:#{ENV['PKG_CONFIG_PATH']}"
@@ -22,9 +29,17 @@ find_header('libxml/xpathInternals.h', '/include/libxml2', '/usr/include/libxml2
 FileUtils.cp_r(Dir.glob("#{LASEM_DIR}/*"), File.dirname(__FILE__))
 File.delete(File.join(File.dirname(__FILE__), 'lasemrender.c'))
 
+# TODO: we need to clear out the build dir that's erroneously getting packaged
+# this causes problems, as Linux installation is expecting OS X output
+if File.directory?(MTEX2MML_BUILD_DIR) && !File.exist?(ROOT_TMP)
+  FileUtils.rm_rf(MTEX2MML_BUILD_DIR)
+end
+FileUtils.mkdir_p(MTEX2MML_BUILD_DIR)
+
 # build mtex2MML library
-Dir.chdir(MTEX2MML_DIR) do
-  system 'make'
+Dir.chdir(MTEX2MML_BUILD_DIR) do
+  system 'cmake ..'
+  system 'make mtex2MML_static'
 end
 
 FileUtils.cp_r(Dir.glob(File.join(MTEX2MML_DIR, 'build', '*.{a,h}')), File.dirname(__FILE__))
