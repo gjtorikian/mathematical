a22195d60515ba63f7ab9245b077b924439d0a7b
Garen Torikian <gjtorikian@gmail.com>
2015-06-12 18:43:05 -0700

    Assert no STDERR messages on the longer tests

--- test/mathematical/mathjax_test.rb
+++ test/mathematical/mathjax_test.rb
@@ -9,7 +9,11 @@ class Mathematical::MathJaxTest < MiniTest::Test
   Dir["#{MATHJAX_TEX_DIR}/**/*.txt"].each do |tex|
     define_method "test_#{tex}" do
       tex_contents = File.read(tex)
-      data = render_svg.render(tex_contents)
+      data = nil
+
+      assert_silent do
+        data = render_svg.render(tex_contents)
+      end
 
       write_sample(tex_contents, File.basename(tex, '.txt')) if ENV['MATHEMATICAL_GENERATE_SAMPLE']
 
